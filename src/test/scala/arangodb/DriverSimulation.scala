5ff885a10da7739cf82aba042a0cd816f6a7cfce
michele <michele@cheergg.com>
2019-10-28 18:39:44 +0100

    getDocument perf test

--- src/test/scala/arangodb/DriverSimulation.scala
+++ src/test/scala/arangodb/DriverSimulation.scala
@@ -1,23 +1,41 @@
 package arangodb
 
+import java.util.UUID
+
+import com.arangodb.entity.BaseDocument
 import com.arangodb.{ArangoDB, Protocol}
 import io.gatling.core.Predef._
 
 class DriverSimulation extends Simulation {
 
+  private val docKey = "myKey-" + UUID.randomUUID().toString
+
   private val arango = new ArangoDB.Builder()
-    .useProtocol(Protocol.VST)
+    .host("127.0.0.1", 8529)
+    .maxConnections(20)
+        .useProtocol(Protocol.VST)
 //    .useProtocol(Protocol.HTTP_VPACK)
-//    .useProtocol(Protocol.HTTP_JSON)
+    //    .useProtocol(Protocol.HTTP_JSON)
     .build()
 
-  private val scn = scenario("getVersion").repeat(100000) {
-    exec(
-      new ArangoActionBuilder("getVersion", () => {
-        arango.getVersion()
-      })
-    )
+  private val collection = arango.db().collection("myColl")
+
+  if (!collection.exists()) {
+    collection.create()
   }
 
-  setUp(scn.inject(atOnceUsers(1)))
+  if (!collection.documentExists(docKey)) {
+    collection.insertDocument(new BaseDocument(docKey))
+  }
+
+  private val scn = scenario("getDocument")
+    .repeat(10000) {
+      exec(
+        new ArangoActionBuilder("getDocument", () => {
+          collection.getDocument(docKey, classOf[BaseDocument])
+        })
+      )
+    }
+
+  setUp(scn.inject(atOnceUsers(50)))
 }
